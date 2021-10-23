05f0a4c42b0535adbaa08c40897cefb77c968dca
Zakaria-Shahen <zikoshahen1@gmial.com>
2021-10-23 13:45:04 +0200

    Get Start

---
+++ schema.md
@@ -0,0 +1,14 @@
+
+### Component
+
+1. book-image
+2. book-info
+3. book-change status
+4. Book
+5. Books list
+6. layout list
+7. layout main 
+8. search Book (add Book)
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { BrowserRouter, Route } from "react-router-dom"
+import AppOld from './AppOld'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<BrowserRouter>
+    <Route exact path='/old' component={AppOld} />
+    <Route exact path='/' component={App} />
+</BrowserRouter>, document.getElementById('root'))
