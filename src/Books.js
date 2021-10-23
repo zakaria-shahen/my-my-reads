9d7e77cadb67b7995108ba06679e3a135ef034ea
Zakaria-Shahen <zikoshahen1@gmial.com>
2021-10-23 23:28:24 +0200

    fix components

--- src/Books.js
+++ src/Books.js
@@ -3,17 +3,22 @@ import React from 'react'
 import Book from './Book'
 
 const Books = props => {
-    return (<div className="bookshelf">
-        <h2 className="bookshelf-title">{props.category}</h2>
-        <div className="bookshelf-books">
-            <ol className="books-grid">
-                {
-                    props.data.map((book) => <li key={book.id}><Book data={book} changeCategory={props.changeCategory} /></li>)
-                }
+    return (
+        <div className="list-books-content">
+            <div>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">{props.category}</h2>
+                    <div className="bookshelf-books">
+                        <ol className="books-grid">
+                            {
+                                props.data.map((book) => <li key={book.id}><Book data={book} changeCategory={props.changeCategory} /></li>)
+                            }
 
-            </ol>
+                        </ol>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
     )
 }
 
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -4,32 +4,24 @@ import { Link } from "react-router-dom"
 import Books from './Books'
 
 const ListBooks = props => {
+    const category = [{ name: 'Currently Reading', id: 'currentlyReading' },
+    { name: 'Want to Reading', id: 'wantToRead' },
+    { name: 'Read', id: 'read' }]
+
+
     return (
         <div className="list-books">
             <div className="list-books-title">
                 <h1>MyReads</h1>
             </div>
-            <div className="list-books-content">
-                <div>
-                    <Books data={props.books.filter((book) => book.category === 'currentlyReading')}
+            {
+                category.map((element) => (
+                    <Books key={element.id} data={props.books.filter((book) => book.category === element.id )}
                         changeCategory={props.changeCategory}
-                        category={"Currently Reading"} />
-                </div>
-            </div>
-            <div className="list-books-content">
-                <div>
-                    <Books data={props.books.filter((book) => book.category === 'wantToRead')}
-                        changeCategory={props.changeCategory}
-                        category={"Want to Reading"} />
-                </div>
-            </div>
-            <div className="list-books-content">
-                <div>
-                    <Books data={props.books.filter((book) => book.category === 'read')}
-                        changeCategory={props.changeCategory}
-                        category={" read "} />
-                </div>
-            </div>
+                        category={element.name} />
+                ))
+            }
+
             <div className="open-search">
                 <Link to='/'><button>Add a book</button></Link>
             </div>
